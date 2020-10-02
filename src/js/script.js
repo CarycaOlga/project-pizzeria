f2db35e6a67fe07876a575c30abda303c882a51b
Natalia Lemańczyk <nataliaolga2@gmail.com>
2020-10-02 15:31:28 +0200

    Remove active class from article

--- src/js/script.js
+++ src/js/script.js
@@ -72,9 +72,16 @@
 
       /* create element using utils.createElementFromHTML */
 
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
       /* find menu container */
 
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
       /* add element to menu */
+
+      menuContainer.appendChild(thisProduct.element);
+
     }
   }
 
