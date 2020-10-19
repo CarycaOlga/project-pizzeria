227600c2bfa93ab5276adeffb29832476e686ca5
Natalia Lemańczyk <nataliaolga2@gmail.com>
2020-10-19 11:11:50 +0200

    create new file DatePicker.js till define constructor

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,6 @@
 import AmountWidget from './AmountWidget.js';
 import {templates, select} from '../settings.js';
+import DatePicker from './DatePicker.js';
 
 class Booking {
   constructor(bookingContainer) {
@@ -19,12 +20,14 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
   }
   initWidgets() {
     const thisBooking = this;
 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   }
 }
 
